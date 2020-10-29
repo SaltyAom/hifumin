d27bd25367f316e742baaccc4b15c0f3dcf15fef
SaltyAom <aomkirby123@gmail.com>
2020-10-30 01:17:17 +0700

    :tada: feat - clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,11 @@
+// @ts-nocheck
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+const clarity = `(function(c,l,a,r,i,t,y){
+	c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+	t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+	y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "3vmuanjtw5");`
 
 class MyDocument extends Document {
 	render() {
@@ -27,6 +34,9 @@ class MyDocument extends Document {
 					/>
 					<meta name="format-detection" content="telephone=no" />
 					<meta name="apple-touch-fullscreen" content="yes" />
+					<script type="text/javascript" dangerouslySetInnerHTML={{
+						__html: clarity
+					}} />
 				</Head>
 				<body>
 					<Main />
