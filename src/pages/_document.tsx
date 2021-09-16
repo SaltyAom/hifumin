d403964423e900e4375f996511d5efd786e64e6f
SaltyAom <aomkirby123@gmail.com>
2021-09-16 18:03:34 +0700

    :tada: feat: add pwa assets

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,29 @@ class OpenerDocument extends Document {
 							/>
 						</>
 					) : null}
+
+					<link rel="manifest" href="/assets/app/manifest.json" />
+					<meta name="mobile-web-app-capable" content="yes" />
+
+					<meta name="application-name" content="Opener Studio" />
+					<meta name="mssmarttagspreventparsing" content="true" />
+					<meta
+						name="msapplication-window"
+						content="width=1366;height=768"
+					/>
+
+					<meta name="apple-mobile-web-app-capable" content="yes" />
+					<meta name="apple-mobile-web-app-title" content="Opener" />
+					<link
+						rel="apple-touch-icon"
+						href="/assets/app/icon/apple-icon.jpg"
+					/>
+					<meta
+						name="apple-mobile-web-app-status-bar-style"
+						content="default"
+					/>
+					<meta name="format-detection" content="telephone=no" />
+					<meta name="apple-touch-fullscreen" content="yes" />
 				</Head>
 				<body>
 					<Main />
