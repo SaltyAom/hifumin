3554c251e05f9114264838fa7119af2acfbc5da0
SaltyAom <aomkirby123@gmail.com>
2021-11-19 23:26:27 +0700

    :wrench: fix: incorrect color chip

--- src/components/atoms/chip/index.tsx
+++ src/components/atoms/chip/index.tsx
@@ -36,13 +36,13 @@ const Chip: ChipComponent = ({
 			type="button"
 			className={combine(
 				tw`inline-flex flex-row items-center appearance-none text-xl py-1 border border-solid dark:border-gray-600 rounded-full transition-colors`,
-				active
-					? tw`text-white dark:text-gray-200 border-black bg-black focus:bg-black dark:bg-gray-600 dark:focus:bg-gray-600`
-					: tw`text-gray-700 dark:text-gray-200 border-gray-200 bg-transparent focus:bg-gray-100 dark:focus:bg-gray-700`,
 				removable ? tw`pl-3 pr-1` : tw`px-3`,
 				onClick
-					? tw`hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer`
+					? tw`dark:hover:bg-gray-700 cursor-pointer`
 					: tw`cursor-default`,
+				active
+					? tw`text-white dark:text-gray-200 border-black bg-black focus:bg-black dark:bg-gray-600 dark:focus:bg-gray-600 `
+					: tw`text-gray-700 dark:text-gray-200 border-gray-200 bg-transparent focus:bg-gray-100 dark:focus:bg-gray-700`,
 				className
 			)}
 			onClick={handleClick}
