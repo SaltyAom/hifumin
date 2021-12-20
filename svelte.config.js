5484afce5694fc79e2fba251ce706eb21da9e97a
saltyaom <saltyaom@gmail.com>
2021-12-20 22:03:15 +0700

    :tada: feat: using netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-netlify'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
