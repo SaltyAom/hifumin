bff95f72c9753704ba00330f05bb0c2be4ea5880
saltyaom <saltyaom@gmail.com>
2022-04-17 21:59:41 +0700

    :tada: exp: cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-cloudflare'
 import preprocess from 'svelte-preprocess'
 
 import WindiCSS from 'vite-plugin-windicss'
