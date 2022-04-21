c93ba670181300b124c0074c583b764a9a87defc
saltyaom <saltyaom@gmail.com>
2022-04-21 23:54:44 +0700

    :tada: feat: add ame punch

---
+++ src/routes/test.svelte
@@ -0,0 +1,10 @@
+<!-- This specific URL have been DDOS by 12M request in 5 minutes -->
+<main class="flex flex-col justify-center items-center gap-2 w-full h-app">
+    <img
+        class="w-48 h-48 rounded object-center object-fill"
+        title="Don't even think about it"
+        src="/assets/images/ame.webp"
+        alt="Ame punch"
+    />
+    <h1 class="text-xl font-medium text-gray-700 dark:text-gray-300">Stop it, go touch grass</h1>
+</main>
