4799b75146496366af4fe19bf52dbdc62e2b778d
saltyaom <saltyaom@gmail.com>
2022-04-22 22:34:32 +0700

    :tada: feat: migration

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,4 @@
-<script context="module" lang="ts">
+<!-- <script context="module" lang="ts">
     import { client } from '@saltyaom/gq'
 
     client.config('https://api.hifumin.app')
@@ -25,4 +25,16 @@
 <Nprogress />
 <AppBar />
 <slot />
-<BottomBar />
+<BottomBar /> -->
+
+<script lang="ts">
+    import 'virtual:windi.css'
+    import '../styles/global.sass'
+</script>
+
+<main class="flex flex-col justify-center items-center w-full h-screen max-w-xs mx-auto gap-2">
+    <img class="w-[128px] h-[128px] object-cover object-center" src="/assets/images/hifumi.webp" alt="Hifumi" />
+    <h1 class="text-3xl text-gray-700 dark:text-gray-400 font-medium my-2">Maintainance</h1>
+    <p class="text-gray-500 dark:text-gray-500 text-center">We are upgrading Hifumin infra and security against DDOS attack.</p>
+    <p class="text-gray-500 dark:text-gray-500 text-center">Hifumin will be available in 2 hours, thanks for your patient and understanding</p>
+</main>
