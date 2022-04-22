90a954c16b6adef31002fdf2df7ef329f140829f
saltyaom <saltyaom@gmail.com>
2022-04-23 02:47:40 +0700

    :tada: feat: back online

---
+++ src/lib/atoms/maintainance.svelte
@@ -0,0 +1,19 @@
+<main
+    class="flex flex-col justify-center items-center w-full h-screen max-w-xs mx-auto gap-2"
+>
+    <img
+        class="w-[128px] h-[128px] object-cover object-center"
+        src="/assets/images/hifumi.webp"
+        alt="Hifumi"
+    />
+    <h1 class="text-3xl text-gray-700 dark:text-gray-400 font-medium my-2">
+        Maintainance
+    </h1>
+    <p class="text-gray-500 dark:text-gray-500 text-center">
+        We are upgrading Hifumin infra and security against DDOS attack.
+    </p>
+    <p class="text-gray-500 dark:text-gray-500 text-center">
+        Hifumin will be available in 2 hours, thanks for your patient and
+        understanding
+    </p>
+</main>
