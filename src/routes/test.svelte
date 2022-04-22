9f9b4c410f9a9685cdabb510a311650170e6ef93
saltyaom <saltyaom@gmail.com>
2022-04-22 10:59:47 +0700

    :wrench: fix: remove env

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,27 +1,3 @@
-<!-- ? This specific URL have been DDOS by 12M request in 5 minutes -->
-<script context="module">
-    import { isWhiteList } from '$lib/server'
-
-    export async function load({ request, fetch, session }) {
-        const { addr } = session
-
-        if (addr && !isWhiteList(addr))
-            // We can return a favor if client open the connection
-            for (let i = 0; i <= 5; i++) fetch(`http://${addr}`).catch(() => {})
-
-        return {
-            status: 200,
-            props: {
-                addr
-            }
-        }
-    }
-</script>
-
-<script lang="ts">
-    export let addr
-</script>
-
 <main class="flex flex-col justify-center items-center gap-2 w-full h-app">
     <img
         class="w-48 h-48 rounded object-center object-fill"
@@ -30,10 +6,6 @@
         alt="Ame punch"
     />
     <h1 class="text-xl font-medium text-gray-700 dark:text-gray-300">
-        Stop it
-        {#if addr}
-            {addr}
-        {/if}
-        touch some grass
+        Stop it, touch some grass
     </h1>
 </main>
