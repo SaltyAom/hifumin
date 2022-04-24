e707fd30826b10dc6f13fba1d5981cca8ce45560
saltyaom <saltyaom@gmail.com>
2022-04-24 15:20:08 +0700

    :wrench: fix: non-blocking pending on concurrent request

--- src/lib/gql/index.ts
+++ src/lib/gql/index.ts
@@ -2,17 +2,14 @@ import gql, { client } from '@saltyaom/gql'
 import InMemoryCache from '@saltyaom/gql-inmemory-cache'
 import LocalCache from '@saltyaom/gql-local-cache'
 
-import { isServer } from '$lib/utils'
-
 client.config('https://api.hifumin.app', {
     plugins: [
-        isServer
-            ? InMemoryCache({
-                  ttl: 60 * 30
-              })
-            : LocalCache({
-                  ttl: 60 * 60 * 3
-              })
+        InMemoryCache({
+            ttl: 60 * 30
+        }),
+        LocalCache({
+            ttl: 60 * 60 * 3
+        })
     ]
 })
 
